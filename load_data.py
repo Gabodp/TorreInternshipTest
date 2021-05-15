be3a0f4a3ccf8cbc7548a1bc63d7ca5201ec7ece
Gabodp <gabparedes@hotmail.com>
2021-05-15 17:21:17 -0500

    3rd commit

--- load_data.py
+++ load_data.py
@@ -5,8 +5,15 @@ df = pd.read_csv(csv_file_name)
 
 job_board = df['job_board']
 
+# rows of dataframe
+num_rows = df.shape[0]
+print(num_rows)
+
 # reed is the only value in the job_board column
 print(job_board.unique())
 
 job_titles = df['job_title'].unique()
+num_unique_titles = len(job_titles)
+
 print(job_titles)
+print(num_unique_titles)
