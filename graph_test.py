dafe74a0db6c42dbb67cec89e094ca661de78ef6
Gabodp <gabparedes@hotmail.com>
2021-05-15 22:03:56 -0500

    6th Commit

--- graph_test.py
+++ graph_test.py
@@ -1,5 +1,6 @@
 from py2neo import Graph
 from py2neo import Node
+from py2neo import Relationship
 
 graph = Graph("bolt://localhost:7687", auth=('neo4j', 'password'))
 
@@ -15,3 +16,9 @@ coke = Node("Manufacturer", name="Coca Cola")
 pepsi = Node("Manufacturer", name="Pepsi")
 
 graph.create(nicole | drew | mtdew | cokezero | coke | pepsi)
+
+graph.create(Relationship(nicole, "LIKES", cokezero))
+graph.create(Relationship(nicole, "LIKES", mtdew))
+graph.create(Relationship(drew, "LIKES", mtdew))
+graph.create(Relationship(coke, "MAKES", cokezero))
+graph.create(Relationship(pepsi, "MAKES", mtdew))
